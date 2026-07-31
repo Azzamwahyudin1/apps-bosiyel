481135ddcb29dc40a73c240dbfd27b49243e6e5e
Azzamwahyudin1 <stokkentangstore227@gmail.com>
2026-07-31 13:39:51 +0700

    add number phone in new card partner

--- src/constant/partners.js
+++ src/constant/partners.js
@@ -1,60 +1,63 @@
 // src/constants/partners.js
 
 export const PARTNERS = [
-    {
-        id: 1,
-        name: "REKAY STORE",
-        tag: "PATNER RESMI IYEL",
-        image: "https://res.cloudinary.com/dsqr42j4/image/upload/v1785421706/copy_of_whatsapp_image_2026-07-30_at_114927_1_iktovw.jpg",
-        contactLink: "https://wa.me/62882003130966"
-    },
-    {
-        id: 2,
-        name: "KALL STORE",
-        tag: "PATNER RESMI IYEL",
-        image: "https://res.cloudinary.com/dsqr42j4/image/upload/v1785383495/WhatsApp_Image_2026-07-30_at_09.27.39_nn51jr.jpg",
-        contactLink: "https://wa.me/085246087202"
-    },
-    {
-        id: 3,
-        name: "SLOT KOSONG",
-        tag: "PATNER RESMI IYEL",
-        image: "https://placehold.co/150x150/000000/FFFFFF?text=STLOT+KOSONG",
-        contactLink: "#"
-    },
-    {
-        id: 4,
-        name: "SLOT KOSONG",
-        tag: "PATNER RESMI IYEL",
-        image: "https://placehold.co/150x150/000000/FFFFFF?text=STLOT+KOSONG",
-        contactLink: "#"
-    },
-    {
-        id: 5,
-        name: "SLOT KOSONG",
-        tag: "PATNER RESMI IYEL",
-        image: "https://placehold.co/150x150/000000/FFFFFF?text=STLOT+KOSONG",
-        contactLink: "#"
-    },
-    {
-        id: 6,
-        name: "SLOT KOSONG",
-        tag: "PATNER RESMI IYEL",
-        image: "https://placehold.co/150x150/000000/FFFFFF?text=STLOT+KOSONG",
-        contactLink: "#"
-    },
-    {
-        id: 7,
-        name: "SLOT KOSONG",
-        tag: "PATNER RESMI IYEL",
-        image: "https://placehold.co/150x150/000000/FFFFFF?text=STLOT+KOSONG",
-        contactLink: "#"
-    },
-    {
-        id: 8,
-        name: "SLOT KOSONG",
-        tag: "PARTNER RESMI IYEL",
-        image: "https://placehold.co/150x150/000000/FFFFFF?text=STLOT+KOSONG",
-        contactLink: "#"
-    },
+  {
+    id: 1,
+    name: "REKAY STORE",
+    tag: "PATNER RESMI IYEL",
+    image:
+      "https://res.cloudinary.com/dsqr42j4/image/upload/v1785421706/copy_of_whatsapp_image_2026-07-30_at_114927_1_iktovw.jpg",
+    contactLink: "https://wa.me/62882003130966",
+  },
+  {
+    id: 2,
+    name: "KALL STORE",
+    tag: "PATNER RESMI IYEL",
+    image:
+      "https://res.cloudinary.com/dsqr42j4/image/upload/v1785383495/WhatsApp_Image_2026-07-30_at_09.27.39_nn51jr.jpg",
+    contactLink: "https://wa.me/085246087202",
+  },
+  {
+    id: 3,
+    name: "DOXX STORE",
+    tag: "PATNER RESMI IYEL",
+    image:
+      "https://res.cloudinary.com/dsqr42j4/image/upload/v1785479838/WhatsApp_Image_2026-07-31_at_06.38.39_hhfef0.jpg",
+    contactLink: "https://wa.me/083177586763",
+  },
+  {
+    id: 4,
+    name: "SLOT KOSONG",
+    tag: "PATNER RESMI IYEL",
+    image: "https://placehold.co/150x150/000000/FFFFFF?text=STLOT+KOSONG",
+    contactLink: "#",
+  },
+  {
+    id: 5,
+    name: "SLOT KOSONG",
+    tag: "PATNER RESMI IYEL",
+    image: "https://placehold.co/150x150/000000/FFFFFF?text=STLOT+KOSONG",
+    contactLink: "#",
+  },
+  {
+    id: 6,
+    name: "SLOT KOSONG",
+    tag: "PATNER RESMI IYEL",
+    image: "https://placehold.co/150x150/000000/FFFFFF?text=STLOT+KOSONG",
+    contactLink: "#",
+  },
+  {
+    id: 7,
+    name: "SLOT KOSONG",
+    tag: "PATNER RESMI IYEL",
+    image: "https://placehold.co/150x150/000000/FFFFFF?text=STLOT+KOSONG",
+    contactLink: "#",
+  },
+  {
+    id: 8,
+    name: "SLOT KOSONG",
+    tag: "PARTNER RESMI IYEL",
+    image: "https://placehold.co/150x150/000000/FFFFFF?text=STLOT+KOSONG",
+    contactLink: "#",
+  },
 ];
